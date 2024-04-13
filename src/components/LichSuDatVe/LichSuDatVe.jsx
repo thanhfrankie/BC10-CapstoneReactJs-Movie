7660d383c308ee96f26dec54b91b8a147b85c255
Khanh <kquoc29200@gmail.com>
2024-04-14 00:09:49 +0700

    update

--- src/components/LichSuDatVe/LichSuDatVe.jsx
+++ src/components/LichSuDatVe/LichSuDatVe.jsx
@@ -10,7 +10,8 @@ import {
 import "../LichSuDatVe/lichSuDatVe.scss";
 import { useDispatch } from "react-redux";
 import useResponsive from "../../hooks/useResponsive";
-
+import Header from "../../layout/Header/Header";
+import Footer from "../../layout/Footer/Footer";
 const LichSuDatVe = ({ infoUser }) => {
   const { isMobile, isTablet, isDesktop } = useResponsive();
   const userLocal = getLocalStorage("user");
@@ -37,72 +38,82 @@ const LichSuDatVe = ({ infoUser }) => {
   }, []);
 
   return (
-    
-    <div className=" history_item flex ">
-      <div className="container w-90 font-bold text-3xl">
-        <div className="grid info_content">
-          <div className="info_item flex bg-red-500 text-white">
-            <h2 className="mr-8 ml-5">Thông Tin Tài Khoản</h2>
-            <a href="">
-              <i className="fa-thin fa-pencil"></i>
-            </a>
+    <div>
+      <div>
+        <Header />
+      </div>
+      <div className=" history_item flex ">
+        <div className=" border_left container w-90 font-bold text-3xl">
+          <div className="grid info_content">
+            <div className="info_item flex bg-red-500 text-white">
+              <h2 className="mr-8 ml-5">Thông Tin Tài Khoản</h2>
+              <a href="">
+                <i className="fa-thin fa-pencil"></i>
+              </a>
+            </div>
+            <div>
+              <h2 className="font-medium text-2xl mb-10 ml-5">
+                Tài Khoản:{" "}
+                <span className="text-green-600">
+                  {thongTinNguoiDung.taiKhoan}
+                </span>
+              </h2>
+              <h2 className="font-medium text-2xl mb-10 ml-5">
+                Mật Khẩu:{" "}
+                <span className="text-green-600">
+                  {thongTinNguoiDung.matKhau}
+                </span>
+              </h2>
+              <h2 className="font-medium text-2xl mb-10 ml-5">
+                Họ Tên:{" "}
+                <span className="text-green-600">
+                  {thongTinNguoiDung.hoTen}
+                </span>
+              </h2>
+              <h2 className="font-medium text-2xl mb-10 ml-5">
+                Email:{" "}
+                <span className="text-green-600">
+                  {thongTinNguoiDung.email}
+                </span>
+              </h2>
+            </div>
           </div>
-          <div>
-            <h2 className="font-medium text-2xl mb-10 ml-5">
-              Tài Khoản:{" "}
-              <span className="text-green-600">
-                {thongTinNguoiDung.taiKhoan}
-              </span>
-            </h2>
-            <h2 className="font-medium text-2xl mb-10 ml-5">
-              Mật Khẩu:{" "}
-              <span className="text-green-600">
-                {thongTinNguoiDung.matKhau}
-              </span>
-            </h2>
-            <h2 className="font-medium text-2xl mb-10 ml-5">
-              Họ Tên:{" "}
-              <span className="text-green-600">{thongTinNguoiDung.hoTen}</span>
-            </h2>
-            <h2 className="font-medium text-2xl mb-10 ml-5">
-              Email:{" "}
-              <span className="text-green-600">{thongTinNguoiDung.email}</span>
-            </h2>
+        </div>
+        <div className="container w-90  text-3xl lichSu_item  ">
+          <div className="  text-white info_item1 font-bold">
+            <h2 className="text-left  ml-5">Lịch Sử Đặt Vé</h2>
+          </div>
+          <div className="lichSu_content ">
+            {datVe.map((ve, index) => (
+              <div className=" lichsu_item" key={index}>
+                <h2 className="font-medium text-3xl mb-5 ml-5">{ve.tenPhim}</h2>
+                <h3 className=" text_item  mb-5 ml-5 font-semibold text-green-600">
+                  Ngày Đặt:{" "}
+                  <span className="text-green-600 text-base font-medium">
+                    {moment(ve.ngayDat).format("HH:mm:ss DD/MM/YYYY")}
+                  </span>
+                </h3>
+
+                <h3 className=" text_item  mb-5 ml-5 font-semibold text-green-600">
+                  Mã Vé: <span>{ve.maVe}</span>
+                </h3>
+                <h3 className=" text_item  mb-5 ml-5 font-semibold text-green-600">
+                  Danh Sách Ghế:{" "}
+                  <span>
+                    {" "}
+                    {ve.danhSachGhe.map((ghe) => ghe.tenGhe).join(", ")}
+                  </span>
+                </h3>
+                {/* Tiếp tục render các thông tin khác của vé ở đây */}
+              </div>
+            ))}
           </div>
         </div>
       </div>
-      <div className="container w-90  text-3xl lichSu_item  ">
-        <div className="  text-white info_item1 font-bold">
-          <h2 className="text-left  ml-5">Lịch Sử Đặt Vé</h2>
-        </div>
-        <div className="lichSu_content ">
-          {datVe.map((ve, index) => (
-            <div className=" lichsu_item" key={index}>
-              <h2 className="font-medium text-3xl mb-5 ml-5">{ve.tenPhim}</h2>
-              <h3 className=" text_item  mb-5 ml-5 font-semibold text-green-600">
-                Ngày Đặt:{" "}
-                <span className="text-green-600 text-base font-medium">
-                  {moment(ve.ngayDat).format("HH:mm:ss DD/MM/YYYY")}
-                </span>
-              </h3>
-
-              <h3 className=" text_item  mb-5 ml-5 font-semibold text-green-600">
-                Mã Vé: <span>{ve.maVe}</span>
-              </h3>
-              <h3 className=" text_item  mb-5 ml-5 font-semibold text-green-600">
-                Danh Sách Ghế:{" "}
-                <span>
-                  {" "}
-                  {ve.danhSachGhe.map((ghe) => ghe.tenGhe).join(", ")}
-                </span>
-              </h3>
-              {/* Tiếp tục render các thông tin khác của vé ở đây */}
-            </div>
-          ))}
-        </div>
+      <div>
+        <Footer />
       </div>
     </div>
-   
   );
 };
 
--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -4,16 +4,7 @@ import { NavLink } from "react-router-dom";
 import { getLocalStorage } from "../../utils/util";
 import { Link } from "react-scroll";
 
-
-
-
 const Header = () => {
-
-
-
-  
-  
-  
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const userLocal = getLocalStorage("user");
   useEffect(() => {
@@ -38,7 +29,6 @@ const Header = () => {
             </span>
           </NavLink>
           <div className="flex items-center lg:order-2">
-         
             {isLoggedIn ? (
               <div className="flex items-center justify-center">
                 <p>{userLocal.hoTen}</p>
@@ -51,35 +41,31 @@ const Header = () => {
               </div>
             ) : (
               <div>
-              <NavLink
-                to="/sign-in"
-                className="text-gray-800 bg-slate-400 hover:bg-gray-500  hover:text-white focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
-              >
-                Đăng nhập
-              </NavLink>
-              <NavLink
-            to="/sign-up"
-            className="text-gray-800 bg-slate-400 hover:bg-gray-500  hover:text-white focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
-          >
-            Đăng Ký
-          </NavLink>
+                <NavLink
+                  to="/sign-in"
+                  className="text-gray-800 bg-slate-400 hover:bg-gray-500  hover:text-white focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
+                >
+                  Đăng nhập
+                </NavLink>
+                <NavLink
+                  to="/sign-up"
+                  className="text-gray-800 bg-slate-400 hover:bg-gray-500  hover:text-white focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
+                >
+                  Đăng Ký
+                </NavLink>
               </div>
-              
-            )  }
-           
+            )}
           </div>
-          <div
-           
-          >
+          <div>
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
                 <Link
-                 activeClass="active"
-                 to="lichChieu"
-                 spy={true}
-                 smooth={true}
-                 offset={-70}
-                 duration={500}
+                  activeClass="active"
+                  to="lichChieu"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
                   className="block  py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-cyan-600 lg:p-0"
                   aria-current="page"
                 >
@@ -88,12 +74,12 @@ const Header = () => {
               </li>
               <li>
                 <Link
-                 activeClass="active"
-                 to="cumRap"
-                 spy={true}
-                 smooth={true}
-                 offset={-70}
-                 duration={500}
+                  activeClass="active"
+                  to="cumRap"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
                   className="block  py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-cyan-600 lg:p-0"
                   aria-current="page"
                 >
